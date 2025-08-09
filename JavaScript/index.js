f1970c4d90a1b5a5cec929ebbe8a6cf8b60abb4a
FelipeCarquio <148922884+FelipeCarquio@users.noreply.github.com>
2025-08-08 22:47:43 -0300

    Ajustando o index
    Criando o menu e o menu do usuário

---
+++ JavaScript/index.js
@@ -0,0 +1,15 @@
+function menu() {
+    if (links.style.display == 'flex') {
+        links.style.display = 'none'
+    } else {
+        links.style.display = 'flex'
+    }
+}
+
+function perfil() {
+    if (perfilUser.style.display == 'flex') {
+        perfilUser.style.display = 'none'
+    } else {
+        perfilUser.style.display = 'flex'
+    }
+}
