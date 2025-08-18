ca6ed0663f73952bfad14fd64ee366686b59bd41
FelipeCarquio <148922884+FelipeCarquio@users.noreply.github.com>
2025-08-18 20:13:34 -0300

    algumas alterações

---
+++ JavaScript/cadastro.js
@@ -0,0 +1,59 @@
+function menu() {
+    if (links.style.display == 'flex') {
+        links.style.display = 'none'
+    } else {
+        links.style.display = 'flex'
+    }
+}
+
+function perfil() {
+    if (perfilUser.style.display == 'flex') {
+        perfilUser.style.display = 'none'
+    } else {
+        perfilUser.style.display = 'flex'
+    }
+}
+
+const openButtons = document.querySelectorAll('.cadastro');
+
+ openButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modalId = button.getAttribute('data-modal');
+        const modal = document.getElementById(modalId);
+
+         modal.showModal();
+    });
+ });
+
+ const closeButtons = document.querySelectorAll('.close');
+
+ closeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modalId = button.getAttribute('data-modal');
+        const modal = document.getElementById(modalId);
+
+         modal.close();
+    });
+ });
+
+ const openButton = document.querySelectorAll('.demitir');
+
+ openButton.forEach(button => {
+    button.addEventListener('click', () => {
+        const modalId = button.getAttribute('data-modal');
+        const modal = document.getElementById(modalId);
+
+         modal.showModal();
+    });
+ });
+
+  const closeButton = document.querySelectorAll('.naoexcluir');
+
+ closeButton.forEach(button => {
+    button.addEventListener('click', () => {
+        const modalId = button.getAttribute('data-modal');
+        const modal = document.getElementById(modalId);
+
+         modal.close();
+    });
+ });
